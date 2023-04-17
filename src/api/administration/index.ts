ff692ab005e4050757cea043fa13d31be47e73d9
keshavsaini2607 <skeshav162@gmail.com>
2023-04-18 00:00:30 +0530

    admin data show

--- src/api/administration/index.ts
+++ src/api/administration/index.ts
@@ -5,7 +5,7 @@ import { AdministrationDataProps } from "./types";
 export const getAdministrationData = async (props: any) => {
    try {
       let {page, page_size} = props?.queryKey[1];
-      const response = await server.get(`/api/v2/users?page=${page}&page_size=${page_size}`);
+      const response = await server.get(`/api/v2/users?page=${page}&page_size=${500}`);
       return response.data;
    } catch (error) {
       handleError(error);
